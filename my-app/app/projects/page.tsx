6f420f8bcd797cdfd5588560d3a20ddfa61bb2dd
Alex Rendler <alexrendler@yahoo.com>
2025-10-31 12:32:06 -0700

    UI Fix

--- my-app/app/projects/page.tsx
+++ my-app/app/projects/page.tsx
@@ -421,7 +421,7 @@ const ProjectsPage = () => {
         {filteredProjects.map((project, index) => (
           <motion.div
             key={index}
-            className="relative group"
+            className="relative group self-start"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: index * 0.1 }}
